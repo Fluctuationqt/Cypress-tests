9581c5107aebf124d910ffe66c240634d3654a13
Ivo Yakov <Outrageousxqt@gmail.com>
2021-08-18 14:12:04 +0300

    Update sample-test.spec.js

--- cypress/integration/tests/sample-test.spec.js
+++ cypress/integration/tests/sample-test.spec.js
@@ -1,5 +1,13 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
+context('Cypress Sample Spec', () => {
+  beforeEach(() => {
+    cy.visit('http://trial-dirigible.ingress.aws.promart.shoot.canary.k8s-hana.ondemand.com/')
+  })
+	
+  it('Search Dirigible on Google', () => {
+    cy.get('form').find('[type="text"]').type('dirigible')
+	
+	cy.get('form').find('[type="submit"]').click()
+	
+	cy.url().should('include', '/services/v4/web/ide/')
   })
 })
